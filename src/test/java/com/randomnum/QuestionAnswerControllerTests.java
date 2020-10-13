8b015a8fe00ff52cae27059d547be92e9af960dc
Shiva.Narayanan <Shiva.Narayanan@MLBTAL-105807.in.marlabs.com>
2020-10-13 15:45:21 +0530

    test case base structure

--- src/test/java/com/randomnum/QuestionAnswerControllerTests.java
+++ src/test/java/com/randomnum/QuestionAnswerControllerTests.java
@@ -1,12 +1,36 @@
 package com.randomnum;
 
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Question.class)
+@SpringBootTest(classes = RandomnumqueApplication.class)
 @WebAppConfiguration
 public class QuestionAnswerControllerTests {
+	
+	private MockMvc mvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() {
+		this.mvc = webAppContextSetup(webApplicationContext).build();
+	}
+	
+	@Test
+	public void successfulQuestion() throws Exception {
+		
+	}
+		
 	}
